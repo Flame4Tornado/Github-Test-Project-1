f352f9a85d19cf5e6f4690c1cb9ca8d6e3a45aa1
Adam Gulmantowicz <adam.gulmantowicz@interia.pl>
2019-06-09 17:31:07 +0200

    Updates

---
+++ precache-manifest.000b1a8681461ec9d9505c9a9dbc7893.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad5416a6a3cb46d12bc028a5493ac193",
+    "url": "/Github-Test-Project-1/index.html"
+  },
+  {
+    "revision": "88139bebe8b9d02a1de8",
+    "url": "/Github-Test-Project-1/static/css/main.18b4c99a.chunk.css"
+  },
+  {
+    "revision": "6dec2f3586fc98017433",
+    "url": "/Github-Test-Project-1/static/js/2.0d473664.chunk.js"
+  },
+  {
+    "revision": "88139bebe8b9d02a1de8",
+    "url": "/Github-Test-Project-1/static/js/main.bbb5974b.chunk.js"
+  },
+  {
+    "revision": "d37c95eba8e27363ae9c",
+    "url": "/Github-Test-Project-1/static/js/runtime~main.c8cd43c9.js"
+  },
+  {
+    "revision": "e9cf5e5c897cf0c26aa36d97aa80848d",
+    "url": "/Github-Test-Project-1/static/media/img1.e9cf5e5c.jpg"
+  },
+  {
+    "revision": "b81767192cc7c118749a8e6eb5816fff",
+    "url": "/Github-Test-Project-1/static/media/img2.b8176719.jpg"
+  },
+  {
+    "revision": "f5022e8386b8f5a59cb90fed60b93477",
+    "url": "/Github-Test-Project-1/static/media/img3.f5022e83.jpg"
+  }
+]);
